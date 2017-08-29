75f4d128a79b6f75be33d43bbc97320c201b9c38
aa-neg <karnold@nos-MacBook.local>
2017-08-29 19:49:30 +1000

    Removing commented code

--- src/main/java/app/EndpointController.java
+++ src/main/java/app/EndpointController.java
@@ -7,8 +7,6 @@ import javax.validation.Valid;
 import app.MailService;
 import lombok.extern.slf4j.Slf4j;
 import model.MailRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EndpointController {
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private MailService mailService;
@@ -30,9 +27,7 @@ public class EndpointController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/sendMail")
-    // public MailResponse sendMail(@RequestBody HttpServletRequest request, response) {
     public boolean sendMail(@Valid @RequestBody MailRequest request) {
-        log.info("here is our request {}", request);
         return mailService.sendMail(request);
     }
 }
